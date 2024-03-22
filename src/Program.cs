1d8234652d778086bfee7c163cb00c8cae1129fc
niltor <zpty@outlook.com>
2024-03-22 15:40:50 +0800

    添加博客的搜索功能

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using Blog;
+using Blog.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,6 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<SearchService>();
 
 await builder.Build().RunAsync();
 
---
+++ src/Services/SearchService.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Services;
+
+public class SearchService
+{
+    public Action<string>? OnSearch;
+
+    public void SearchAsync(string? key)
+    {
+        OnSearch?.Invoke(key);
+    }
+}
