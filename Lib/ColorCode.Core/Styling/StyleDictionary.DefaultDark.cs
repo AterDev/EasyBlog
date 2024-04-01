140efb568cf25cb2e4ad57917fa1ae4ff0618482
niltor <zpty@outlook.com>
2024-04-01 18:19:34 +0800

    improve csharp code highlight

--- Lib/ColorCode.Core/Styling/StyleDictionary.DefaultDark.cs
+++ Lib/ColorCode.Core/Styling/StyleDictionary.DefaultDark.cs
@@ -337,6 +337,17 @@ public partial class StyleDictionary
                     Foreground = Teal,
                     ReferenceName = "variable"
                 },
+                new Style(ScopeName.Function)
+                {
+                    Foreground = Teal,
+                    ReferenceName = "function"
+                },
+                new Style(ScopeName.Symbol)
+                {
+                    Foreground = VSDarkKeyword,
+                    ReferenceName = "symbol"
+                }
+
             ];
         }
     }
